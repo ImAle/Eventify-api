c3054e9ee22da44045f71358480eb624885bfff7
Álvaro <alvarocc948@gmail.com>
2024-12-13 20:39:57 +0100

    Botón para ir a eventos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,6 +11,7 @@
         <div class="container mx-auto flex justify-between items-center px-6">
             <a href="/" class="text-2xl font-bold text-gray-700">Eventify</a>
             <nav>
+            
                 @if (Route::has('login'))
                     <div class="flex space-x-4">
                         @auth
@@ -29,6 +30,11 @@
     <main class="container mx-auto mt-10 text-center">
         <h1 class="text-4xl font-semibold text-gray-700">Bienvenido a Eventify</h1>
         <p class="mt-4 text-gray-600">La solución profesional para la gestión de tus eventos.</p>
+        <!--<a href="{{ url('/events') }}" class="text-2xl font-bold text-gray-700">Consulta los eventos disponibles</a>-->
+        <br>
+        <button onclick="window.location.href='{{ url('/events') }}'" class="btn btn-primary font-bold text-gray-700">
+            Consulta los eventos disponibles aquí.
+        </button>
     </main>
 </body>
 </html>
